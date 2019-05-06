66734fd5515a07cdfc671f4c66750b4737e86852
arthurlapertosa <arthurlisboa@cpejr.com.br>
2019-05-06 03:38:41 -0300

    Criei a pasta javascripts e terminei a pagina "midTrainings!

---
+++ public/javascripts/cpf.js
@@ -0,0 +1,15 @@
+function fMasc(objeto,mascara) {
+    obj=objeto
+    masc=mascara
+    setTimeout("fMascEx()",1)
+}
+function fMascEx() {
+    obj.value=masc(obj.value)
+}
+function mCPF(cpf){
+    cpf=cpf.replace(/\D/g,"")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d)/,"$1.$2")
+    cpf=cpf.replace(/(\d{3})(\d{1,2})$/,"$1-$2")
+    return cpf
+}
