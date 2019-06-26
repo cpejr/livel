b4e4a09521e7c2fbb417b8e0b4eca6e2ba102288
arthurlapertosa <arthurlisboa@cpejr.com.br>
2019-06-25 22:27:47 -0300

    Adequando indentação

--- models/requisitions.js
+++ models/requisitions.js
@@ -2,26 +2,11 @@ require('dotenv').config();
 const request = require('request-promise');
 
 class requisitions{
-    login(AlunoID, AlunoPwd){
-        return new Promise((resolve) =>{
-            request({
-              method: 'GET',
-              url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.AUTH_TOKEN}&Metodo=alunoLogin&AlunoID=${AlunoID}&AlunoPwd=${AlunoPwd}`,
-              json: true,
-              resolveWithFullResponse: true
-            }).then((response) => {
-              resolve (response.body);
-            }).catch((err) => {
-              console.log(err);
-              resolve (1);
-            });
-          });
-    }
-    treinoSemana(){
+  login(AlunoID, AlunoPwd){
       return new Promise((resolve) =>{
           request({
             method: 'GET',
-            url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.AUTH_TOKEN}&Metodo=treinoSemana`,
+            url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.AUTH_TOKEN}&Metodo=alunoLogin&AlunoID=${AlunoID}&AlunoPwd=${AlunoPwd}`,
             json: true,
             resolveWithFullResponse: true
           }).then((response) => {
@@ -32,6 +17,21 @@ class requisitions{
           });
         });
   }
+  treinoSemana(){
+    return new Promise((resolve) =>{
+        request({
+          method: 'GET',
+          url: `http://fitgroup.com.br/livel/livel_app.php?AuthToken=${process.env.AUTH_TOKEN}&Metodo=treinoSemana`,
+          json: true,
+          resolveWithFullResponse: true
+        }).then((response) => {
+          resolve (response.body);
+        }).catch((err) => {
+          console.log(err);
+          resolve (1);
+        });
+      });
+    }
 }
 
 
