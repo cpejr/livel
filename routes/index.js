187ad30593becb6c346b39c6d3a4ff797db342d2
Isadora Queiroz <isadoraqueiroz@cpejr.com.br>
2019-08-02 18:23:39 -0300

    Coloquei o gradiente(famoso degradê) na parte profile-resultado e no body. E o body já está com a foto do corpo e todas as suas partes

--- routes/index.js
+++ routes/index.js
@@ -83,7 +83,7 @@ router.get('/profile', function(req,res, next){
   res.render('profile', {title: 'Profile', foto_perfil, aluno_treinos, layout: 'layoutMenuBars'})
 });
 
-router.get('/testgraf', function(req,res, next){
-  res.render('testgraf', {title: 'testgraf', foto_perfil, aluno_treinos, layout: 'layoutMenuBars'})
+router.get('/body', function(req,res, next){
+  res.render('body', {title: 'body', foto_perfil, aluno_treinos, layout: 'layoutMenuBars'})
 });
 module.exports = router;
