507709888355ab42392f559be93d0657337000c0
Isadora Queiroz <isadoraqueiroz@cpejr.com.br>
2019-08-13 17:19:30 -0300

    Tentativas do crono

--- public/javascripts/midTraining.js
+++ public/javascripts/midTraining.js
@@ -1,61 +1,62 @@
 // https://jsfiddle.net/Daniel_Hug/pvk6p/
 var state = "pause";
+var n=0;
+function cronometro(n){
+  var h1 = document.getElementsByTagName('h1')[0],
+  start = document.getElementById('comeca'),
+  stop = document.getElementById('para'),
+  t,time;
+  time=n;
+
+  function add() {
+    time--;
+    minutes = parseInt(time / 60, 10);
+    seconds = parseInt(time % 60, 10);
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    h1.textContent = minutes + ":" + seconds;
+    console.log("restante"+time);
+    timer();
+  }
+  function timer() {
+    t = setTimeout(add, 1000);
+  }
+  timer();
+  /* Start button */
+  start.onclick = timer;
+  return time;
+  stop.onclick = function() {
+    clearTimeout(t);
+  }
+}
 
 function buttonPlayPress(){
+var time,rest=0;
     if (state == "pause"){
         state = "play";
         var button2 = d3.select("#button_play").classed('btn-change-purple', true); //atribui o roxo do botao
         var button = d3.select("#button_play").classed('btn-success', false); //tira verde ao botao
-        button.select("i").attr('class', "fa fa-pause btn-size"); //muda o icone para pause
-        var result,timet;
-        var timeleft = 5*60;
-        n++;
-        var n=0;
-        var h1 = document.getElementsByTagName('span')[0];
-          start = document.getElementById('start'),
-          stop = document.getElementById('stop'),
-          clear = document.getElementById('clear'),
-          timeleft=300,seconds = 0,minutes=0;
-          function add() {
-              timeleft=timeleft-1;
-              minutes = parseInt(timeleft/60);
-              seconds = parseInt(timeleft%60);
-              h1.textContent =(minutes < 10 ? "0" + minutes : minutes) + ":" + (seconds < 10 ? "0" + seconds : seconds);
-
-              timer();
-          }
-          function timer() {
-              t = setTimeout(add, 1000);
-          }
-          timer();
-
-
-          /* Start button */
-          start.onclick = timer;
-
-          /* Stop button */
-          stop.onclick = function() {
-              clearTimeout(t);
-          }
-
-          /* Clear button */
-          clear.onclick = function() {
-              h1.textContent = "00";
-              seconds = 0; minutes = 0; hours = 0;
-      }
+        button.select("i").attr('class', "fa fa-pause btn-size"); //muda o icone para paus
+        console.log("valor na start"+n);
+        if(n==0){
+          console.log("ENTRA NO START");
+          time=60*2;
+          rest=cronometro(time);
+        }
     }
     else if(state=='play' || state=='resume'){
       state = 'pause';
       var button = d3.select("#button_play").classed('btn-success', true);
       var button2 = d3.select("#button_play").classed('btn-change-purple', false);
       button2.select("i").attr('class', "fa fa-play btn-size");
-      function pare(){
-        console.log('entro');
-        clearInterval(downloadTimer);
-      };
-      pare();
-      display = document.querySelector('.atividade');
+      console.log("valor no stop"+n);
+      if(n==1){
+        rest=cronometro(time);
+        console.log("ENTRA NO STOP");
+        console.log(rest);
 
+      }
     }
-    console.log(n);
+    n++;
+
 }
