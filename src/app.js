3e62499c2d2e2192b608d2d2b793ae52200b67ea
Carolina <carolinaalvares@cpejr.com.br>
2020-04-13 14:04:26 -0300

    mais mudancas
    mais mudancas

--- src/app.js
+++ src/app.js
@@ -13,10 +13,10 @@ const session = require('express-session');
 require('dotenv').config;
 
 //Routes
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const coachRouter = require('./routes/coach');
-const coacheesRouter = require('./routes/coachees')
+const indexRouter = require('../routes/index');
+const usersRouter = require('../routes/users');
+const coachRouter = require('../trash/coach');
+const coacheesRouter = require('../trash/coachees')
 const app = express();
 
 // View Engine Setup
---
+++ trash/routes/coach.js
@@ -0,0 +1,10 @@
+// var express = require('express');
+// var router = express.Router();
+
+// module.exports = router;
+
+// // $( ".userimg-coach" ).click(function goToURL() {
+// //     location.href='/trainingTypes');
+// //   });
+
+ 
